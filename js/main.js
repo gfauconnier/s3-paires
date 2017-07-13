4777f86b4aa2df39f666061dd8e45881fb7e3379
Greg Fauconnier <g.fauconnier@hotmail.fr>
2017-07-13 10:02:27 +0200

    index.html : removed p used for testing added one to display result - main.js : added a 'score'(need to rest it when finished)

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
-var flag = 0,
-tempCard = [0, 0];
-var arr = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6];
+var flag = 0, compteur = 0, nbTentatives = 0,
+  tempCard = [0, 0];
+var arr = ["FFFFFF", "FFFFFF", "000000", "000000", "FF0000", "FF0000", "00FF00", "00FF00", "0000FF", "0000FF", "555555", "555555", "AAAAAA", "AAAAAA"];
 
 var ps = document.getElementsByTagName("p");
 
-var btns = document.getElementsByTagName("button");
-Array.from(btns).forEach(function(element) {element.addEventListener('click', testCard)});
+var btns = document.getElementsByTagName("div");
+Array.from(btns).forEach(function(element) {
+  element.addEventListener('click', testCard)
+});
 
 cardReset();
 
@@ -19,26 +21,37 @@ function cardReset() {
   }
   arr.forEach(function(item, index) {
     ps[index].innerHTML = item;
+    btns[index].style.backgroundColor = "#" + arr[index];
   });
 }
 
 
 function testCard() {
   var nb = this.id;
-  if (flag == 0) {
-    tempCard[0] = ps[nb].innerHTML;
-    tempCard[1] = this.id;
-    document.getElementById(this.id).classList.add("dhidd");
-    flag++;
-  } else if (tempCard[0] == ps[nb].innerHTML) {
-    document.getElementById(this.id).classList.add("dhidd");
-    flag = 0;
-  } else {
-    document.getElementById(this.id).classList.add("dhidd");
-    flag = 0;
-    setTimeout(function(){
-      document.getElementById(tempCard[1]).classList.remove("dhidd");
-      document.getElementById(nb).classList.remove("dhidd");
-    },500);
+  if (this.classList.contains("dhidd")) {
+
+    if (flag == 0) {
+      tempCard[0] = ps[nb].innerHTML;
+      tempCard[1] = this.id;
+      document.getElementById(this.id).classList.remove("dhidd");
+      flag++;
+    } else if (tempCard[0] == ps[nb].innerHTML) {
+      document.getElementById(this.id).classList.remove("dhidd");
+      flag = 0;
+      compteur++;
+      nbTentatives++;
+    } else {
+      document.getElementById(this.id).classList.remove("dhidd");
+      flag = 0;
+      setTimeout(function() {
+        document.getElementById(tempCard[1]).classList.add("dhidd");
+        document.getElementById(nb).classList.add("dhidd");
+      }, 500);
+      nbTentatives++;
+    }
+  }
+  console.log(compteur)
+  if (compteur == 7) {
+    document.getElementById("resultat").innerHTML = "Vous avez gagné en " + nbTentatives + " tentatives";
   }
 }
