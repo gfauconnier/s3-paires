7f71bd95657fffb8431260596e4beea1ecfc1d1b
Greg Fauconnier <g.fauconnier@hotmail.fr>
2017-07-13 11:35:55 +0200

    index.html : added section and article for display purpose - main.css : added flex on section + some style to cards - main.js : finished won function > replayable game

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 var flag = 0, compteur = 0, nbTentatives = 0,
   tempCard = [0, 0];
-var arr = ["FFFFFF", "FFFFFF", "000000", "000000", "FF0000", "FF0000", "00FF00", "00FF00", "0000FF", "0000FF", "555555", "555555", "AAAAAA", "AAAAAA"];
+var arr = ["#FFFFFF", "#FFFFFF", "#FF8888", "#FF8888", "#FF0000", "#FF0000", "#00FF00", "#00FF00", "#0000FF", "#0000FF", "#8888FF", "#8888FF", "#88FF88", "#88FF88"];
 
-var ps = document.getElementsByTagName("p");
-
-var btns = document.getElementsByTagName("div");
-Array.from(btns).forEach(function(element) {
+document.getElementById("replay").addEventListener('click', function(){won(0)});
+var cards = document.getElementsByTagName("div");
+Array.from(cards).forEach(function(element) {
   element.addEventListener('click', testCard)
 });
 
@@ -20,8 +19,7 @@ function cardReset() {
     arr[rand] = temp;
   }
   arr.forEach(function(item, index) {
-    ps[index].innerHTML = item;
-    btns[index].style.backgroundColor = "#" + arr[index];
+    cards[index].style.backgroundColor = arr[index];
   });
 }
 
@@ -31,11 +29,11 @@ function testCard() {
   if (this.classList.contains("dhidd")) {
 
     if (flag == 0) {
-      tempCard[0] = ps[nb].innerHTML;
+      tempCard[0] = this.style.backgroundColor;
       tempCard[1] = this.id;
       document.getElementById(this.id).classList.remove("dhidd");
       flag++;
-    } else if (tempCard[0] == ps[nb].innerHTML) {
+    } else if (tempCard[0] == this.style.backgroundColor) {
       document.getElementById(this.id).classList.remove("dhidd");
       flag = 0;
       compteur++;
@@ -49,9 +47,25 @@ function testCard() {
       }, 500);
       nbTentatives++;
     }
+
   }
-  console.log(compteur)
+
   if (compteur == 7) {
+    won(1);
+  }
+}
+
+function won(repl) {
+  if (repl == 0) {
+    document.getElementById("replay").classList.toggle("d-none");
+    document.getElementById("resultat").innerHTML = "";
+    Array.from(cards).forEach(function(element){element.classList.toggle("dhidd"); console.log(element);});
+    cardReset();
+  }
+  else {
     document.getElementById("resultat").innerHTML = "Vous avez gagné en " + nbTentatives + " tentatives";
+    document.getElementById("replay").classList.toggle("d-none");
+    nbTentatives = 0;
+    compteur = 0;
   }
 }
